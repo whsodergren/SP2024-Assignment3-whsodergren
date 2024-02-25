7f4c3a8328e9db85b240df7e854b7792a793cd9a
whsodergren <whsodergren@crimson.ua.edu>
2024-02-25 17:07:13 -0600

    Add project files.

---
+++ SP2024-Assignment3-whsodergren/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SP2024_Assignment3_whsodergren.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
